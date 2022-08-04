c5111f2b73d6c765a07b64a420fbf02bc1c3a2b6
Sylvain Guilbaud <Sylvain.Guilbaud@intersystems.com>
2022-08-04 13:11:59 +0200

    testcase

--- shell/import.sh
+++
@@ -1,8 +0,0 @@
-export filename="response-import.json"
-echo "deleting " $filename
-rm -f $filename
-curl -d "@data-import.json" \
--H "Content-Type: application/json" \
--X POST http://localhost:12773/common/import \
--o $filename \
---user "_system:SYS"
