28cb71e9e494ca3769aaf53b9b69e375c214f644
Sylvain Guilbaud <Sylvain.Guilbaud@intersystems.com>
2022-08-09 15:15:25 +0200

    testcase

--- src/shell/import-many-small.sh
+++
@@ -1,8 +0,0 @@
-export filename="response-import-many-small.json"
-echo "deleting " $filename
-rm -f $filename
-curl -d "@data-many-small.json" \
--H "Content-Type: application/json" \
--X POST http://localhost:12773/common/importjson \
--o $filename \
---user "_system:SYS"
