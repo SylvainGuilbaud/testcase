c490ef527c7998c633d2296e0e35e084a5b1aa0f
Sylvain Guilbaud <Sylvain.Guilbaud@intersystems.com>
2022-08-09 13:47:19 +0200

    testcase

--- src/shell/import-large-txt.sh
+++
@@ -1,8 +0,0 @@
-export filename="response-import-large-txt.json"
-echo "deleting " $filename
-rm -f $filename
-curl -d "@data-import-large.json" \
--H "Content-Type: application/json" \
--X POST http://localhost:12773/common/import \
--o $filename \
---user "_system:SYS"
