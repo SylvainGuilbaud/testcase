282b7e17e07fbaf36e9cf8994797731d9875cef9
Sylvain Guilbaud <Sylvain.Guilbaud@intersystems.com>
2022-08-09 14:28:22 +0200

    testcase

--- src/shell/import-38-small.sh
+++
@@ -1,8 +0,0 @@
-export filename="response-import-38-small.json"
-echo "deleting " $filename
-rm -f $filename
-curl -d "@data-38-small.json" \
--H "Content-Type: application/json" \
--X POST http://localhost:12773/common/import \
--o $filename \
---user "_system:SYS"
